2724bf9603803ae6e1d518de4ae3c4cf82a4a12f
Xingsheng Wei <xw2815@columbia.edu>
2022-05-31 00:43:40 -0400

    Scrape mp3 from EOP

--- dl_EOP_midi_script.py
+++ dl_EOP_midi_script.py
@@ -44,33 +44,10 @@ def get_midi_from_EOP(cookie, begin, end):
                     f.write(midi.content)
         print(f'number: {i} done')
 
-def get_num_score_from_EOP(cookie, begin, end):
-    headers = {
-        'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
-        'Cookie':cookie
-        }  
-    save_dir = 'EOP_data'
-    for i in range(begin,end): #14212
-        url = f"https://www.everyonepiano.cn/PDF-{i}.html"
-        page = requests.get(url=url,headers=headers)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        dl_page = ''
-        for item in soup.find_all('a',{'class': 'btn btn-success btn-block BtnDown'}): 
-            if item["href"][0:6]=='/Music':
-                #print(item["href"])
-                file_name = item["href"][15:].replace('/','-') + '.pdf'
-                print(file_name)
-                dl_page = 'https://www.everyonepiano.cn' + item["href"]
-                #print(dl_page)
-                pdf = requests.get(url=dl_page,headers=headers)
-                with open(os.path.join(save_dir,file_name), 'wb') as f:
-                    f.write(pdf.content)
-        print(f'number: {i} done')
-
 
 if __name__ == '__main__':
     args = get_args()
     cookie = "menunew=0-0-0-0; PHPSESSID=de1cd01801c326a22326ad8d4aff821b; think_language=en-US; username=szdqt; password=Qweszxc%40123; remember=1; huiyuan=5237ij2KdH%2FFO9Ze9uqvyyhDbmyfW87VgIPLpdqkm9fj%2F1vc75yM8ZU; FWE_getuser=szdqt; FWE_getuserid=253026; login_mima=c8122c2b69c95522513555570479f2c1"
     test_EOP_cookie(cookie)
-    get_num_score_from_EOP(cookie, args.begin, args.end)
+    get_midi_from_EOP(cookie, args.begin, args.end)
     
---
+++ dl_EOP_mp3_script.py
@@ -0,0 +1,95 @@
+import requests  
+from bs4 import BeautifulSoup
+import json  
+import os
+import argparse
+from multiprocessing import Pool
+import pickle as pk
+
+
+HEADERS = {
+        'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0",
+        'Cookie':"menunew=0-0-0-0; PHPSESSID=de1cd01801c326a22326ad8d4aff821b; think_language=en-US; username=szdqt; password=Qweszxc%40123; remember=1; huiyuan=5237ij2KdH%2FFO9Ze9uqvyyhDbmyfW87VgIPLpdqkm9fj%2F1vc75yM8ZU; FWE_getuser=szdqt; FWE_getuserid=253026; login_mima=c8122c2b69c95522513555570479f2c1"
+        }  
+
+SAVE_DIR = '/media/xingshengwei/Seagate Portable Drive/scrape/EOP_data_mp3'
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--begin', type=int, default=0) 
+    parser.add_argument('--end', type=int, default=14212) 
+    return parser.parse_args()
+
+
+def test_EOP_cookie():
+    url = "https://www.everyonepiano.cn/Myspace" 
+    page = requests.get(url=url,headers=HEADERS)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    for item in soup.find_all('p',{'class': 'UserName'}): #'id': 'search-result'
+        print(item.text)
+
+
+
+def scrape_one(url_tuple):
+    #for i,file in enumerate(os.listdir(SAVE_DIR)):
+
+    print(f'number: {url_tuple[0]} start')
+    page = requests.get(url=url_tuple[1],headers=HEADERS)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    dl_page = ''
+    for item in soup.find_all('head'): 
+        song_name = item.title.text
+        #print('song_name:', song_name)
+    for item in soup.find_all('audio'):
+        #print('item:', item) 
+        dl_page = item["src"]
+
+        #print(item["src"])
+        file_name = song_name.replace('/','-') + '.mp3'
+        print('file_name:', file_name)
+        #print('dl_page:', dl_page)
+        mp3 = requests.get(url=dl_page,headers=HEADERS)
+        with open(os.path.join(SAVE_DIR,file_name), 'wb') as f:
+            f.write(mp3.content)
+
+    log_path = os.path.join(SAVE_DIR,'log.pkl')
+    with open(log_path, "rb") as f:
+        log = pk.load(f)
+    with open(log_path, "wb") as f:
+        log.append(url_tuple[0])
+        pk.dump(log,f)
+
+    print(f'number: {url_tuple[0]} done')
+
+
+def get_num_score_from_EOP(begin, end):
+    
+    log_path = os.path.join(SAVE_DIR,'log.pkl')
+    if not os.path.exists(log_path):
+        with open(log_path, "wb") as f:
+            pk.dump([0],f)
+    with open(log_path, "rb") as f:
+        log = pk.load(f)
+        print(log)
+
+    all_url_tuples = []
+    for i in range(begin,end): #14212
+        url = f"https://www.everyonepiano.cn/Mp3-{i}.html"
+        url_tuple = (i,url)
+        all_url_tuples.append(url_tuple)
+    for index in log:
+        all_url_tuples[index] = (-1,'')
+    for i,url_tuple in reversed(list(enumerate(all_url_tuples))):
+        if url_tuple[0] == -1: all_url_tuples.pop(i)
+
+    with Pool(20) as p:
+        p.map(scrape_one, all_url_tuples)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    test_EOP_cookie()
+    get_num_score_from_EOP(args.begin, args.end)
+    #mp3 = requests.get(url='https://play.everyonepiano.cn/015/0014218-172n17nt.mp3',headers=HEADERS)
+    #with open(os.path.join(SAVE_DIR,'test.mp3'), 'wb') as f:
+    #    f.write(mp3.content)
